c44721546acf0ef6c047a789d831dc2a2666d8f9
athuljain <athuljainkj2@gmail.com>
2023-08-14 09:58:12 +0530

    first commit

---
+++ src/IndependanceDay.jsx
@@ -0,0 +1,15 @@
+import "./IndependanceDay.css"
+import "./App.css"
+const IndependanceDay = () => {
+    return (
+      <div className="independence-day-greeting">
+        <h1>Happy Independence Day!</h1>
+        <p>Let's celebrate the freedom and unity of our nation.</p>
+      
+      <div>
+        <p className="P1">athuljain</p>
+      </div>
+      </div>
+    );
+  };
+  export default IndependanceDay
---
+++ src/IndianFlag.jsx
@@ -0,0 +1,27 @@
+import "./IndianFlag.css"
+import "./App.css"
+
+const IndianFlag = () => {
+    return (
+      <div className="indian-flag">
+        <div className="saffron-bar"></div>
+        <div className="white-bar">
+          <div className="chakra">
+            <div className="chakra-circle">
+              <div className="chakra-spoke"></div>
+              <div className="chakra-spoke"></div>
+              <div className="chakra-spoke"></div>
+              <div className="chakra-spoke"></div>
+              <div className="chakra-spoke"></div>
+              <div className="chakra-spoke"></div>
+              <div className="chakra-spoke"></div>
+              <div className="chakra-spoke"></div>
+            </div>
+          </div>
+        </div>
+        <div className="green-bar"></div>
+      </div>
+    );
+  };
+
+export default IndianFlag;
